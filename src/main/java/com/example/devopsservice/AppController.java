cf1baa0e3d7d89c4a721edcc658092237223e307
나유성 <yusung052077@gmail.com>
2026-09-10 15:59:10 +0900

    first

---
+++ src/main/java/com/example/devopsservice/AppController.java
@@ -0,0 +1,12 @@
+package com.example.devopsservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/com/example/devopsservice/DevopsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsServiceApplication.class, args);
+	}
+
+}
